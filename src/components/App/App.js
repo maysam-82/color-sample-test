6d31d1bd7e0f89c7a7440d0dc9a01e15cf6cc708
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-01 18:35:30 +1000

    create function to generate image with colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,49 @@ class App extends React.Component {
 		this.canvasRef = React.createRef();
 	}
 
+	componentDidMount() {
+		this.generateImageTile(32);
+	}
+
+	// Creates image inside canvas according to step size (32px)/
+	// Using canvas will help to have better
+	generateImageTile = (tileSize) => {
+		const htmlCanvas = this.canvasRef.current;
+		const numberOfColumns = htmlCanvas.width / tileSize;
+		const numberOfRows = htmlCanvas.height / tileSize;
+		const context = htmlCanvas.getContext('2d');
+		let colorPletteIndex = 0;
+
+		// Generates tiles with relevant step size
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+				// Most browsers support `createImageData`.
+				const imageData = context.createImageData(tileSize, tileSize);
+				// Iterate through every pixel of tile
+				for (let index = 0; index < imageData.data.length; index += 4) {
+					const { red, green, blue } = this.colorPalette[
+						colorPletteIndex + index / 4
+					];
+					// Red;
+					imageData.data[index + 0] = red;
+					// Green
+					imageData.data[index + 1] = green;
+					// Blue
+					imageData.data[index + 2] = blue;
+					// Alpha Channel
+					imageData.data[index + 3] = 255;
+				}
+				colorPletteIndex = tileSize * tileSize + colorPletteIndex;
+				// Most browsers support `putImageData` method
+				context.putImageData(
+					imageData,
+					columnIndex * tileSize,
+					rowIndex * tileSize
+				);
+			}
+		}
+	};
+
 	render() {
 		return (
 			<div className="app-container">
