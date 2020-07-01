a8bf26eb500402aefe8b03e73c6f3ac33f434075
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-01 18:31:40 +1000

    create canvas and pass react ref

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
+import { generateColorPaletteWithUniqueColors } from './appUtils';
 class App extends React.Component {
+	constructor() {
+		super();
+		// Generate colors array with unique values before mounting of DOM in constructor to normalize data.
+		this.colorPalette = generateColorPaletteWithUniqueColors();
+		// Gets canvas ref from DOM
+		this.canvasRef = React.createRef();
+	}
+
 	render() {
-		return <div>App</div>;
+		return (
+			<div className="app-container">
+				<div className="canvas-container">
+					<canvas
+						id="imageCanvas"
+						width="256"
+						height="128"
+						ref={this.canvasRef}
+					/>
+				</div>
+			</div>
+		);
 	}
 }
 
